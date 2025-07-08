8bc8ef230d634ad3cb5732128862473dae1555cb
Andika Wirapala Fathin Azfar <deutandev@pm.me>
2025-07-08 22:34:55 +0700

    feat(enemy): Add basic mob AI movement towards player
    - Add speed property to mob character
    - Implement player tracking in _ready()
    - Add movement logic in _physics_process()
    - Mob now follows player using direction_to() and move_and_slide()
    - Add new player and environment directories

--- characters/enemies/mob/mob.gd
+++ characters/enemies/mob/mob.gd
@@ -1,1 +1,12 @@
 extends CharacterBody2D
+
+@export var speed := 300.0
+var player
+
+func _ready() -> void:
+	player = get_node("/root/Map/Player")
+
+func _physics_process(delta: float) -> void:
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * speed
+	move_and_slide()
