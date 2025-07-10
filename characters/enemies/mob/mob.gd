149bec086314796943a0da5e60fdb30987fad1dc
Hinakhina <karaniyametta2030@gmail.com>
2025-07-10 23:44:12 +0700

    mob temp health

--- characters/enemies/mob/mob.gd
+++ characters/enemies/mob/mob.gd
@@ -7,3 +7,11 @@ func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * speed
 	move_and_slide()
+
+var health := 100
+
+func take_damage():
+	health -= 50
+	if health <= 0:
+		queue_free()
+		
