bac7f24dd695e75570b4a349655fd92cea3398b9
Hinakhina <karaniyametta2030@gmail.com>
2025-07-10 17:12:27 +0700

    player input and movement

---
+++ characters/player/player.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+var speed = 400
+
+func get_input():
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * speed
+
+func _physics_process(delta: float) -> void:
+	get_input()
+	move_and_slide()
