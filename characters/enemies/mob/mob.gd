f7e2e66e2bce12573d7686baf1b5ac508382b70a
Andika Wirapala Fathin Azfar <deutandev@pm.me>
2025-07-12 23:51:45 +0700

    refactor(enemy, bullet): update stat system

--- characters/enemies/mob/mob.gd
+++ characters/enemies/mob/mob.gd
@@ -1,20 +1,31 @@
 extends CharacterBody2D
 
-@export var speed := 300.0
+@export var max_health: int = 1
+@export var speed: float = 1.0
+@export var damage: int = 1
+
+@export var defense: float = 1.0
+@export var min_damage_taken: float = 1.0
+
 @onready var player = get_node("/root/Map/Player")
 @onready var coin_drop: CoinDropManager = %CoinDropManager
+
+var current_health: int
+
+func _ready():
+	speed = speed * 100.0
+	current_health = max_health
 
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * speed
 	move_and_slide()
 
-var health := 100
-
-func take_damage():
-	health -= 50
-	if health <= 0:
+func take_damage(base_damage: float):
+	var reduced_damage = max(base_damage - defense, min_damage_taken) # avoid having 0 damage
+	current_health -= reduced_damage
+	
+	if current_health <= 0:
 		Game_Stats.add_score(10)
 		coin_drop.drop_coin(global_position)
 		queue_free()
-		
--- pistol/gun_bullet.gd
+++ pistol/gun_bullet.gd
@@ -1,19 +1,22 @@
 extends Area2D
 
-var travelled_distance := 0
+
+@export var damage := 1
+@export var speed := 1000.0
+@export var shoot_range := 1200.0
+
+var travelled_distance := 0.0
 var direction
-const SPEED := 1000
-const RANGE := 1200
 
 func _physics_process(delta: float) -> void:
 	direction = Vector2.RIGHT.rotated(rotation)
-	position += direction * SPEED * delta
-	travelled_distance += SPEED * delta
-	if travelled_distance > RANGE:
+	position += direction * speed * delta
+	travelled_distance += speed * delta
+	if travelled_distance > shoot_range:
 		queue_free()
 
 
 func _on_body_entered(body: Node2D) -> void:
 	queue_free()
 	if body.has_method("take_damage"):
-		body.take_damage()
+		body.take_damage(damage)
