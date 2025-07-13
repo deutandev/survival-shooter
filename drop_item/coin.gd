c329ee4f032e9bc8db5711eaaf2540dc628af300
Andika Wirapala Fathin Azfar <deutandev@pm.me>
2025-07-13 13:38:24 +0700

    refactor(player, coin): move collection logic to coin

--- drop_item/coin.gd
+++ drop_item/coin.gd
@@ -1,12 +1,14 @@
 extends Area2D
 class_name Coin
 
-var value := 0
+var value := 1
 
 func set_value(v: int):
 	value = v
 
-func collect():
-	print_debug("Collected coin worth: ", value)
-	Game_Stats.add_coin(value)
-	queue_free()
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print_debug("Collected coin worth: ", value)
+		Game_Stats.add_coin(value)
+		queue_free()
