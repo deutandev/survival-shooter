685b6d61f586fa8efd15bfee93b8f92d22269533
Andika Wirapala Fathin Azfar <deutandev@pm.me>
2025-07-17 11:47:15 +0700

    feat(player): add player animation

--- pistol/gun.gd
+++ pistol/gun.gd
@@ -3,11 +3,15 @@ extends Area2D
 var enemies_in_range
 var target_enemy
 
+signal shoot_direction_changed(is_facing_right: bool)
+
 func _physics_process(delta: float) -> void:
 	enemies_in_range = get_overlapping_bodies()
 	if enemies_in_range.size() > 0:
 		target_enemy = enemies_in_range.front()
 		look_at(target_enemy.global_position)
+		var is_facing_right = target_enemy.global_position.x > global_position.x
+		emit_signal("shoot_direction_changed", is_facing_right)
 
 func shoot():
 	var bullet = preload("res://pistol/bullet.tscn").instantiate()
