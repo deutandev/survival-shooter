4b07b295b01f3817ca3308be8ac8bdf8ba41d67c
Hinakhina <karaniyametta2030@gmail.com>
2025-07-26 22:17:03 +0700

    Refactoring: randomize map

--- chunksystem/chunk_manager.gd
+++ chunksystem/chunk_manager.gd
@@ -2,65 +2,56 @@ extends Node2D
 class_name ChunkManager
 
 @export var chunk_scene: PackedScene
-@export var chunk_size: Vector2i = Vector2i(5769, 3240)
-@export var load_radius: int = 2 
+@export var chunk_size: Vector2i = Vector2i(5769, 3240)  # Large sprite-based chunk
+@export var chunk_buffer: int = 1  # Radius around player
+@export var available_images: Array[Texture2D]
+@export var player: Node2D
 
-@export var player: CharacterBody2D
 var loaded_chunks: Dictionary = {}
+var last_player_chunk := Vector2i(-999, -999)
+var chosen_image: Texture2D
 
 func _ready():
-	update_loaded_chunks() # Initial load
-	set_process(true)
+	chosen_image = available_images.pick_random()
 
 func _process(_delta):
-	update_loaded_chunks()
-
-func update_loaded_chunks():
-	if not player:
-		return
-
-	var player_chunk = get_chunk_coords(player.global_position)
-
-	# Load surrounding chunks
-	for x in range(-load_radius, load_radius + 1):
-		for y in range(-load_radius, load_radius + 1):
-			var chunk_coords = player_chunk + Vector2i(x, y)
-			if not loaded_chunks.has(chunk_coords):
-				load_chunk(chunk_coords)
-
-	# Unload far-away chunks
-	var to_unload: Array = []
-	for coords in loaded_chunks.keys():
-		if coords.distance_to(player_chunk) > load_radius:
-			to_unload.append(coords)
-
-	for coords in to_unload:
-		unload_chunk(coords)
+	var player_chunk := get_chunk_coords(player.global_position)
+	if player_chunk != last_player_chunk:
+		last_player_chunk = player_chunk
+		update_loaded_chunks()
 
 func get_chunk_coords(pos: Vector2) -> Vector2i:
 	return Vector2i(floor(pos.x / chunk_size.x), floor(pos.y / chunk_size.y))
 
+func update_loaded_chunks():
+	var center := get_chunk_coords(player.global_position)
+	var chunks_to_keep: Dictionary = {}
+
+	for x in range(center.x - chunk_buffer, center.x + chunk_buffer + 1):
+		for y in range(center.y - chunk_buffer, center.y + chunk_buffer + 1):
+			var coords = Vector2i(x, y)
+			chunks_to_keep[coords] = true
+			if not loaded_chunks.has(coords):
+				load_chunk(coords)
+
+	for coords in loaded_chunks.keys():
+		if not chunks_to_keep.has(coords):
+			unload_chunk(coords)
+
 func load_chunk(chunk_coords: Vector2i):
 	var chunk = chunk_scene.instantiate()
-	var world_pos = Vector2(chunk_coords.x * chunk_size.x, chunk_coords.y * chunk_size.y)
-	chunk.position = world_pos
-
-	if chunk.has_method("init_chunk"):
-		chunk.init_chunk(chunk_coords)
 
 	add_child(chunk)
+	var world_pos = Vector2(chunk_coords.x * chunk_size.x, chunk_coords.y * chunk_size.y)
+	chunk.position = world_pos
+	chunk.set_coords(chunk_coords)
+	chunk.set_image(chosen_image)
+
 	loaded_chunks[chunk_coords] = chunk
 
-	# Restore data if any
-	var saved_data = WorldSave.retrieve_data(chunk_coords)
-	if chunk.has_method("load_data"):
-		chunk.load_data(saved_data)
-
 func unload_chunk(chunk_coords: Vector2i):
-	var chunk = loaded_chunks[chunk_coords]
-	if chunk and chunk.has_method("get_data_to_save"):
-		var data = chunk.get_data_to_save()
-		WorldSave.save_chunk(chunk_coords, data)
-
-	chunk.queue_free()
-	loaded_chunks.erase(chunk_coords)
+	if loaded_chunks.has(chunk_coords):
+		var chunk = loaded_chunks[chunk_coords]
+		if is_instance_valid(chunk):
+			chunk.queue_free()
+		loaded_chunks.erase(chunk_coords)
